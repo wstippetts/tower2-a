d5af86726bc8ae128787a3db406b7b8161bc3aaf
Scott Tippetts <wstippetts+github@gmail.com>
2023-06-28 15:55:35 -0600

    some updates to homepage

--- tower2.client/src/components/Navbar.vue
+++ tower2.client/src/components/Navbar.vue
@@ -2,14 +2,14 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+        <img alt="logo" src="../assets/img/Logo.png" height="45" />
       </div>
     </router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
       aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarText">
+    <div class="collapse navbar-collapse bg-drop" id="navbarText">
       <ul class="navbar-nav me-auto">
         <li>
           <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
@@ -34,6 +34,11 @@ export default {
 </script>
 
 <style scoped>
+.bg-drop {
+  background-color: rgba(0, 24, 84, 0.866);
+  border-radius: 10px;
+}
+
 a:hover {
   text-decoration: none;
 }
--- tower2.client/src/pages/HomePage.vue
+++ tower2.client/src/pages/HomePage.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+  <div class="container-fluid bg-primary">
+    <div class="row">
+      <div class="col-12 p-3">
+        <div class="d-flex flex-column align-items-center">
+          <img src="../assets/img/Banner.png" alt="banner" class="img-fluid">
+        </div>
+      </div>
+      <div class="container">
+        <div class="row my-4"></div>
+      </div>
     </div>
+
+
   </div>
 </template>
+
 
 <script>
 export default {
@@ -18,24 +24,5 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
 
-  .home-card {
-    width: 50vw;
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
-}
-</style>
+<style lang="scss" scoped></style>
